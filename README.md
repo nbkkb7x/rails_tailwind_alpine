068a79ed3266b8956c4024ad8b58e3a565d343e0
nbkkb7x <mapope83@gmail.com>
2021-09-07 17:15:08 -0400

    First Commit. Got Alpine & Tailwind (as CDN) running. Fix that.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# rails_tailwind_alpine
-Rails + Tailwind + Alpine Demo
+# Rails + Tailwind + Alpine
+
+This repo is used to store my demo of using Tailwind & AlpineJS on a Ruby on Rails platform. I will be following along with the Udemy Course: (Alpine JS Vs 3 for Beginners)[https://www.udemy.com/course/alpinejs/]. More information t come as I dig in.
+
+
+#### Get Started
+
+
+
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import "@hotwired/turbo-rails"
+import "@rails/activestorage"
+import "alpine-turbo-drive-adapter"
+import Alpine from 'alpinejs'
+import "stylesheets/application.scss"
+
+
+window.Alpine = Alpine
+Alpine.start()
+
+
